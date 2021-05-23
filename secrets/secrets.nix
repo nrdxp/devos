595878f7bc8f893cedad78d4bfbf4bf8352a9385
Timothy DeHerrera <tim.deherrera@iohk.io>
2021-05-23 15:27:43 -0600

    secrets: add my secrets

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,9 +1,29 @@
 let
   # set ssh public keys here for your system and user
-  system = "";
-  user = "";
-  allKeys = [ system user ];
+  root_serval-ws =
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMLrGNYsXDf4jcsAvZvc+YE1pj8yZ/FFd6Eo6amqfmKo root@serval-ws";
+  nrd_serval-ws =
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIML/MziS7kA8u4kzzGJDgUxI849Ydj4DH6CDj/IaWYot nrd@serval-ws";
+  iohk =
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDB5LMY783Srcv4pCfCjcjgug+Xq1EGTLP1AJWugGgXg tim.deherrera@iohk.io";
+  allKeys = [ root_serval-ws nrd_serval-ws ];
+
+  allKeysSecrets = builtins.listToAttrs (map
+    (name: {
+      inherit name;
+      value = { publicKeys = allKeys; };
+    }) [
+    "aws.age"
+    "cachix.age"
+    "cargo.age"
+    "cloudflare.age"
+    "github.age"
+    "gitlab.age"
+    "iohk.age"
+    "nrd.age"
+    "wireguard.age"
+    "root.age"
+  ]);
+
 in
-{
-  "secret.age".publicKeys = allKeys;
-}
+allKeysSecrets
