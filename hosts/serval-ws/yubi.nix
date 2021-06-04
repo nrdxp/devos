e10794d505c6c4fbdbf081c5b725ce9b6bea8e93
Timothy DeHerrera <tim.deh@pm.me>
2021-06-04 14:02:51 -0600

    yubikey pam auth

--- hosts/serval-ws/yubi.nix
+++ hosts/serval-ws/yubi.nix
@@ -2,5 +2,15 @@
   services.pcscd.enable = true;
 
   services.udev.packages = with pkgs; [ yubikey-personalization ];
-  environment.systemPackages = with pkgs; [ yubikey-personalization ];
+  environment.systemPackages = with pkgs; [
+    yubikey-personalization
+    yubico-pam
+    yubikey-manager
+  ];
+
+  security.pam.yubico = {
+    enable = true;
+    debug = false;
+    mode = "challenge-response";
+  };
 }
